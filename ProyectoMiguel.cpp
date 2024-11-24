2436e7880c3a0b22ac28a6c884b9235eb179bed1
Miguel Cano <mcanodomingo@gmail.com>
2024-11-24 17:00:57 +0100

    Agregar archivos de proyecto.

---
+++ ProyectoMiguel.cpp
@@ -0,0 +1,12 @@
+﻿// ProyectoMiguel.cpp: define el punto de entrada de la aplicación.
+//
+
+#include "ProyectoMiguel.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
